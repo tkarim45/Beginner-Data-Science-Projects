290eaa5d21fe45cf29e897aa2ebf220263fe4719
Taimour Abdul Karim <taimourabdulkarim20@gmail.com>
2024-08-05 14:03:23 +0500

    End to End Deployment

--- Email Spam Detection/app.py
+++ Email Spam Detection/app.py
@@ -30,5 +30,17 @@ def predict_api():
     return jsonify(my_prediction.tolist())
 
 
+@app.route("/predict", methods=["POST"])
+def predict():
+    message = request.form.get("email")  # Get the specific form field 'email'
+    print(message)
+    vect = pickled_cv.transform([message]).toarray()  # Ensure message is in a list
+    my_prediction = logmodel.predict(vect)
+    print(my_prediction)
+    return render_template(
+        "home.html", prediction_text="The message is {}".format(my_prediction[0])
+    )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
